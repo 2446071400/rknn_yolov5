f834cb5ed33afc522dab4291ee031e0940396033
JZZ <2446071400@qq.com>
2024-06-13 02:48:25 +0000

    v1.0

---
+++ build/CMakeFiles/yolov5.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jzz/code/rknn_yolov5/utils/file_utils.c" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/utils/file_utils.c.o"
+  "/home/jzz/code/rknn_yolov5/utils/image_drawing.c" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/utils/image_drawing.c.o"
+  "/home/jzz/code/rknn_yolov5/utils/image_utils.c" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/utils/image_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../utils"
+  "../3rdparty/rknpu2/include"
+  "../3rdparty/librga/include"
+  "../3rdparty/jpeg_turbo/include"
+  "../3rdparty/stb_image/include"
+  "/usr/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jzz/code/rknn_yolov5/main.cpp" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/main.cpp.o"
+  "/home/jzz/code/rknn_yolov5/postprocess.cpp" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/postprocess.cpp.o"
+  "/home/jzz/code/rknn_yolov5/yolov5.cpp" "/home/jzz/code/rknn_yolov5/build/CMakeFiles/yolov5.dir/yolov5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../utils"
+  "../3rdparty/rknpu2/include"
+  "../3rdparty/librga/include"
+  "../3rdparty/jpeg_turbo/include"
+  "../3rdparty/stb_image/include"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yolov5.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov5.dir/main.cpp.o"
+  "CMakeFiles/yolov5.dir/yolov5.cpp.o"
+  "CMakeFiles/yolov5.dir/postprocess.cpp.o"
+  "CMakeFiles/yolov5.dir/utils/file_utils.c.o"
+  "CMakeFiles/yolov5.dir/utils/image_drawing.c.o"
+  "CMakeFiles/yolov5.dir/utils/image_utils.c.o"
+  "yolov5.pdb"
+  "yolov5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/yolov5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
